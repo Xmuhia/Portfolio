94c724666078150348a08d16ff6a19f3ff076f52
Brendan Muhia <brendanmuhia@Brendans-MacBook-Pro.local>
2023-12-09 00:00:34 +0300

    Resolve merge conflicts and update Hero component

---
+++ client/client/src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import React from 'react';
+import { Hero, Skills, Projects, Contacts } from './components';
+
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contacts />
+    </div>
+  )
+}
+
+export default App
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,15 +1,19 @@
+// Hero.js
 import React from 'react';
-import ParticlesComponent from './ParticlesComponent';
-import "../index.css"; 
-import imageUrl from '../assets/SpaceBackground.jpeg';
+import Earth from '../assets/SpaceBackground.jpeg';
+import Moonman from '../assets/Moonman.png';
+import '../index.css';
 
 const Hero = () => {
-    return (
-        <div className="hero-container">
-            <img src={imageUrl} className="absolute inset-0 h-full w-full object-cover" />
-            <ParticlesComponent className="particles" /> {/* Add the className here */}
-        </div>
-    );
+  return (
+    <div className="hero-container">
+      <img src={Earth} className="relative inset-0 h-full w-full object-cover" />
+      <img src={Moonman} id="bg"/>
+      <img src="moon.png" id="moon"/>
+      <img src="mountain.png" id="mountain"/>
+      <h2 id="text"> Moon </h2>
+    </div>
+  );
 };
 
 export default Hero;
