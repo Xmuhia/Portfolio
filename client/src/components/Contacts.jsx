b36116525b5cbf0a9e21b51db40daefa29536fed
Brendan Muhia <dotcombrendan@gmail.com>
2023-12-20 17:25:21 +0300

    Adding Click Functions to Contacts

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-
 
 const Contacts = () => {
   return (
@@ -12,9 +10,21 @@ const Contacts = () => {
       <div class="color"></div>
       
       <ul>
-        <li><FaGithub className='icons'/></li>
-        <li><FaLinkedin className='icons'/></li>
-        <li><MdEmail className='icons'/></li>
+        <li>
+          <a href="https://github.com/Xmuhia" target="_blank" rel="noopener noreferrer">
+            <FaGithub className='icons' />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/brendan-muhia" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className='icons' />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:docotcombrendan@gmail.com" target="_blank" rel="noopener noreferrer">
+            <MdEmail className='icons' />
+          </a>
+        </li>
       </ul>
     </section>
   )
